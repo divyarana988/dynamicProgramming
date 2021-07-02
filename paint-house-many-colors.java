c5ef1cdd46f4e3487e254cf6348f6a60ec7ed7ce
DIVYA RANA <divyarana491@gmail.com>
2021-07-02 14:37:38 +0530

    added files

---
+++ paint-house-many-colors.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int k = scn.nextInt();
+        
+        int arr[][] = new int[n][k];
+        
+        for(int i=0; i<n; i++){
+            for(int j=0; j<k; j++){
+                
+                arr[i][j] = scn.nextInt();
+            }
+        }
+     
+        int dp[][] = new int[arr.length][arr[0].length];
+        
+        int least = Integer.MAX_VALUE;
+        int sleast = Integer.MAX_VALUE;
+        
+        for(int j=0; j<arr[0].length; j++){
+            dp[0][j] = arr[0][j];
+            if(arr[0][j]<=least){
+                sleast = least;
+                least = arr[0][j];
+            }
+            else if(arr[0][j]<=sleast){
+                sleast = arr[0][j];
+            }
+        }
+        for(int i=1; i<arr.length; i++){
+            int nleast = Integer.MAX_VALUE;
+            int nsleast = Integer.MAX_VALUE;
+            
+            for(int j=0; j<arr[0].length; j++){
+                if(least==dp[i-1][j]){
+                    dp[i][j] = sleast+arr[i][j];
+                }
+                else{
+                    dp[i][j] = least+arr[i][j];
+                }
+                if(dp[i][j]<= nleast){
+                    nsleast = nleast;
+                    nleast = dp[i][j];
+                }
+                else if(dp[i][j]<=nsleast){
+                    nsleast = dp[i][j];
+                }
+            }
+            least = nleast;
+            sleast = nsleast;
+        }
+        System.out.println(least);
+    }
+    
+}
